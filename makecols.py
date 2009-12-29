c9a39456b15ac0f506f936855fb7479c48df04ed
Andy Elvey <andy@obsidian.(none)>
2009-12-29 22:30:39 +1300

    More work on makecols.py.

--- makecols.py
+++ makecols.py
@@ -7,18 +7,46 @@
 
 import math, string, itertools  
 
+# Helper function to convert a number to a column letter. 
+def num2str(n):
+    #assert isinstance(n,int) and n > 0
+    digits = "-ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    res = []
+    while True:
+        n, r = divmod(n, 26)
+        if r == 0:    # Adjust the quotient and remainder
+            n, r = n-1, 26
+        res[0:0] = digits[r]
+        if n == 0:
+            return "".join(res)
+
+
 class page(object): 
   def __init__(self, scrwidth, defaultwidth): 
      self.collist = [] 
      self.colwidths = [] 
      self.scrwidth = scrwidth 
      self.defaultwidth = defaultwidth 
-     self.numcols = math.floor(self.scrwidth / self.defaultwidth) 
+     self.numcols = int(math.floor(self.scrwidth / self.defaultwidth)) 
      # Populate the lists 
      for i in range(0, self.numcols):
         self.collist.append(chr(65+i)) 
         self.colwidths.append(self.defaultwidth) 
         
+  # Add columns to the lists until we reach the screen width.       
+  def add(self):       
+     # The letter of the current rightmost column.  
+     # Note - need to later update this to cater for multi-letter 
+     # column names. 
+     self.lastcol = self.collist[len(self.collist)-1] 
+     self.newcol = num2str(len(self.collist)+1) 
+     print self.lastcol, self.newcol   
+     
+  # Remove cols from the lists until the width is the screen width 
+  # or less.    
+  def remove(self):     
+     pass 
+          
   # A refresh method to recalculate the number of columns on the 
   # screen when a column has had its width changed.       
   def refresh(self):       
@@ -40,6 +68,7 @@ class page(object):
 a = page(80, 7)
 a.setwidth('C', 4) 
 a.setwidth('E', 11) 
+a.add() 
 a.display() 
 
 
